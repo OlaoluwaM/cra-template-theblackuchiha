13d0cf1d447643272a45b4185546eb7570c88cc3
Olaoluwa Mustapha <jomeemustapha@gmail.com>
2021-02-26 19:27:09 -0600

    feat(renaming): renamed 'general-components' as 'reusables', and converted 'App.js' to 'App.jsx'

---
+++ template/src/reusables/Loading.js
@@ -0,0 +1,80 @@
+import PropTypes from 'prop-types';
+
+import { default as styled, css } from 'styled-components';
+import { m as motion, AnimateSharedLayout } from 'framer-motion';
+
+const variants = {
+  hidden: {
+    opacity: 0,
+    transition: {
+      when: 'afterChildren',
+    },
+  },
+  visible: {
+    opacity: 1,
+    transition: { when: 'beforeChildren' },
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      when: 'afterChildren',
+    },
+  },
+};
+
+const Overlay = styled(motion.div).attrs({
+  'data-testid': 'loader',
+  variants: variants,
+  initial: 'hidden',
+  animate: 'visible',
+  exit: 'exit',
+  key: 'overlay',
+  className: 'loader',
+})`
+  background: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+  width: 100%;
+
+  ${({ isFullscreen }) =>
+    isFullscreen
+      ? css`
+          position: fixed;
+          height: 100%;
+        `
+      : css`
+          position: absolute;
+          height: 45%;
+        `}
+`;
+
+const Spinner = styled(motion.div)`
+  width: 30px;
+  height: 30px;
+  transform-origin: center;
+  border-radius: 100%;
+  border: 5px solid transparent;
+  border-top-color: ${({ theme }) => theme.baseColor};
+  animation: spin 0.5s linear infinite;
+  will-change: transform;
+`;
+
+export default function Loading({ fullscreen = false, layoutId, children }) {
+  const layoutObj = layoutId ? { layoutId } : { layout: true };
+
+  return (
+    <AnimateSharedLayout>
+      <Overlay isFullscreen={fullscreen} {...layoutObj}>
+        {children ?? <Spinner layout />}
+      </Overlay>
+    </AnimateSharedLayout>
+  );
+}
+
+Loading.propTypes = {
+  fullscreen: PropTypes.bool,
+  layoutId: PropTypes.string,
+  children: PropTypes.element,
+};
