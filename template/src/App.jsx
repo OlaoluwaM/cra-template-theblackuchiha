7fb154613e8cf92976b05b516aac1e76ec3f2304
Olaoluwa Mustapha <jomeemustapha@gmail.com>
2021-05-11 21:27:20 +0100

    chore: Made some minor additions to these files
    For the gitIgnore, I added a small fix.
    For the App.jsx, I added a comment for future reference.
    For the index.scss, I removed some unecessary variables

--- template/src/App.jsx
+++ template/src/App.jsx
@@ -5,12 +5,14 @@ import { ThemeProvider } from 'styled-components';
 import { LazyMotion, domAnimation } from 'framer-motion';
 import { Route, Switch, useLocation } from 'react-router-dom';
 
+// Use domMax instead of domAnimation if you plan on having layout animations
+
 export default function App() {
   const location = useLocation();
 
   return (
     <ThemeProvider theme={themeObj}>
-      <LazyMotion features={domAnimation}>
+      <LazyMotion features={domAnimation} strict>
 
         <>
           <Suspense fallback={<p>Loading</p>}>
