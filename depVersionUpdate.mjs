d1f6a4bd117613c00b53134120efbc1a6585040d
Olaoluwa Mustapha <jomeemustapha@gmail.com>
2021-05-11 21:28:58 +0100

    feat: Finally created script to auto update dep versions
    Created script to bump up versions of the dependencies listed
    in the template.json file. Also added this script as an npm
    script

---
+++ depVersionUpdate.mjs
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+import fsPromise from 'fs/promises';
+
+import { exec } from 'child_process';
+import { promisify } from 'util';
+
+const promisifiedExec = promisify(exec);
+
+const normalize = str => (str === '' ? false : true);
+
+const templateJSOnFile = JSON.parse(
+  await fsPromise.readFile('./template.json', { encoding: 'utf-8' })
+);
+
+const dependencies = Object.entries(templateJSOnFile.package.dependencies);
+
+const promiseResults = await Promise.allSettled(
+  dependencies.map(async ([packageName, currVer]) => {
+    try {
+      const { stdout, stderr } = await promisifiedExec(`npm show ${packageName} version`);
+      if (normalize(stderr)) throw stderr;
+      return [packageName, `^${stdout.replace('\n', '')}`];
+    } catch (error) {
+      console.error(error);
+      return [packageName, currVer];
+    }
+  })
+);
+
+const updatedDeps = promiseResults.map(({ value }) => value);
+templateJSOnFile.package.dependencies = Object.fromEntries(updatedDeps);
+
+await fsPromise.writeFile('./template.json', JSON.stringify(templateJSOnFile));
+console.log('Packages updated!');
